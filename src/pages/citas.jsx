3429887bc054c104f580d24466f50735b3918e08
CamiloCuenca <camilocuenca1810@gmail.com>
2025-03-09 01:44:55 -0500

    corrige la sensibilidad de mayúsculas en la importación del componente Footer en Vercel

--- src/pages/citas.jsx
+++ src/pages/citas.jsx
@@ -1,7 +1,7 @@
 import FormularioCita from "../components/FormularioCita";
 import TableCitas from "../components/TableCitas";
 import Header from "../components/Header";
-import Footer from "../components/Footer";
+import Footer from "../components/footer";
 
 export default function Citas() {
     return (
