60b6cfeb1782b0cb2e985ea3d432f98e09830d71
Brandon Montealegre <sneid123@hotmail.com>
2025-03-29 00:22:35 -0500

    mejora al inicio

--- src/components/Inicio.jsx
+++ src/components/Inicio.jsx
@@ -27,7 +27,7 @@ const Inicio = () => {
                     {/* Botón para agendar una cita, al hacer clic redirige a la página de citas */}
                     <button 
                         onClick={() => window.location.href='/citas'}
-                        className="px-6 md:px-10 py-2 text-lg md:text-2xl rounded-md bg-[var(--color-primary)] hover:bg-[var(--color-secondary)] text-white transition duration-300">
+                        className="px-6 md:px-10 py-2 text-lg md:text-2xl rounded-md bg-[var(--color-secondary)] hover:bg-[var(--color-primary)] text-white transition duration-300">
                         Agendar Cita
                     </button>
                 </div>
