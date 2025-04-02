885bac31aa2eb63dd6d0b6090d7d2e92e710f43f
Brandon Montealegre <sneid123@hotmail.com>
2025-04-02 04:52:54 -0500

    se impleneto la nuevas rutas para las nuevas paginas historialMedico y perfil

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,8 @@ import Registro from './pages/registro';
 import ActivarCuenta from './pages/activarCuenta';
 import RecuperarContraseña from './pages/recuperarContraseña';
 import CambiarContraseña from './pages/cambiarContraseña';
+import Perfil from './pages/perfil';
+import HistorialMedico from './pages/historialMedicoUsuario';
 import { Toaster } from 'react-hot-toast';
 
 function App() {
@@ -18,29 +20,35 @@ function App() {
         position="top-center"
         reverseOrder={false}
       />
-      
+
       {/* Se definen las rutas de la aplicación usando el componente Routes */}
       <Routes>
         {/* Ruta principal que carga la página de inicio */}
         <Route path="/" element={<Home />} />
-        
+
         {/* Ruta para la gestión de citas */}
         <Route path="/citas" element={<Citas />} />
-        
+
         {/* Ruta para el inicio de sesión */}
         <Route path="/login" element={<Login />} />
-        
+
         {/* Ruta para el registro de nuevos usuarios */}
         <Route path="/registro" element={<Registro />} />
-        
+
         {/* Ruta para la activación de cuentas */}
         <Route path="/activarCuenta" element={<ActivarCuenta />} />
-        
+
         {/* Ruta para recuperar la contraseña */}
         <Route path="/recuperarContraseña" element={<RecuperarContraseña />} />
-        
+
         {/* Ruta para cambiar la contraseña */}
         <Route path="/cambiarContraseña" element={<CambiarContraseña />} />
+
+        {/* Ruta para el perfil */}
+        <Route path="/perfil" element={<Perfil />} />
+
+        {/* Ruta para el historial médico */}
+        <Route path="/historialMedicoUsuario" element={<HistorialMedico />} />
       </Routes>
     </Router>
   )
